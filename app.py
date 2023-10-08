bc817e71d2ed85bde228415c1c7f042e57a98852
Injinkiki <143388597+Injinkiki@users.noreply.github.com>
2023-10-08 17:06:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+import http.server
+import socketserver
 import subprocess
-
 subprocess.call(['python', 'test.py'])
+
+# 웹 서버를 시작할 포트 번호 설정 (예: 8080)
+port = 8080
+
+# 현재 디렉토리를 웹 서버의 경로로 설정
+handler = http.server.SimpleHTTPRequestHandler
+
+# 웹 서버 시작
+with socketserver.TCPServer(("", port), handler) as httpd:
+    print(f"Serving at port {port}")
+    httpd.serve_forever()
